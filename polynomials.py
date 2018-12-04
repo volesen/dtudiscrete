340df7904f22f1e01af0be2d8a4e548f0e083c8b
albert <albertsgarde@gmail.com>
2018-12-04 19:16:50 +0100

    Fix polynomials

--- polynomials.py
+++ polynomials.py
@@ -1,23 +1,21 @@
-import numpy as np
-
 class Polynomial:
     def __init__(self, coefs):
         self.coefs = coefs
     
     def __getitem__(self, key):
-        return self.coefs[key]
+        return self.coefs[key] if key in self.coefs.keys() else 0
 
     def y(self, x):
         result = 0
-        for exponent, coef in self.coefs:
+        for exponent, coef in self.coefs.items():
             result += coef*(x**exponent)
         return result
     
     def __add__(self, pol):
         result = {}
-        for exponent, coef in self.coefs:
+        for exponent, coef in self.coefs.items():
             result[exponent] = coef
-        for exponent, coef in pol.coefs:
+        for exponent, coef in pol.coefs.items():
             if exponent in result:
                 result[exponent] += coef
             else:
@@ -25,14 +23,25 @@ class Polynomial:
         return Polynomial(result)
     
     def __mul__(self, pol):
-        resultDegree = self.degree() + pol.degree()
+        result_degree = self.degree() + pol.degree()
         result = {}
-        for i in range(0, resultDegree + 1):
+        for i in range(0, result_degree + 1):
             coef = 0
-            for j in range(0, self.degree() + 1):
+            for j in range(0, max(self.degree() + 1, i)):
                 coef += self[j]*pol[i-j]
             result[i] = coef
-        return result
+        return Polynomial(result)
 
     def degree(self):
-        return max(self.coefs.keys)
+        return max(self.coefs.keys())
+
+    def __str__(self):
+        result = ''
+        for exponent, coef in self.coefs.items():
+            if exponent == 0:
+                result += " + " + str(coef)
+            elif exponent == 1:
+                result += " + " + str(coef) + "x"
+            else:
+                result += " + " + str(coef) + 'x^' + str(exponent)
+        return result[3:]
