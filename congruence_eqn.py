3e8a8c14c82fcf617f688d6e8ad34a6f63e0c411
Vincent Olesen <vincolesen@gmail.com>
2018-12-05 17:36:01 +0100

    Implemented single congruence solver with steps

--- congruence_eqn.py
+++ congruence_eqn.py
@@ -1,2 +1,29 @@
+from math import gcd
 from extended_euclidean import egcd
+from inverse import inverse_mod
 
+# TODO: Notes use egcd(n, a) which switches s, t. Fix consistency
+
+def congruence_eqn(a, b, n):
+    ''' Solves for x in "x * a ≡ b (mod n)" with steps '''
+
+    d = gcd(a, n)
+
+    # Base case
+    if d == 1:
+    	c = inverse_mod(a, n, steps = False)
+
+    	return f'By "Theorem 5.8" the equation is equivalent to x ≡ c*b (mod n) where c={c} is the multiplicative inverse of a modulus n. Thus x ≡ {c * b} ≡ {(c * b) % n} (mod {n})'
+
+    # If d does not divide a, there are no solutions
+    if d//b != 0:
+    	return f'For d = gcd({a}, {n}), as d does not divide b, there exists no solutions per "Theorem 5.7"'
+
+    # If d divides b, the eqation is equivalent to solving a' ≡ b' (mod n')
+    a_prime = a//d
+    b_prime = b//d
+    n_prime = n//d
+
+    return congruence_eqn(a_prime, b_prime, n_prime)
+
+print(congruence_eqn(3,5,9))
--- inverse.py
+++ inverse.py
@@ -3,11 +3,11 @@ from extended_euclidean import egcd
 
 
 # TODO: Notes use egcd(n, a) which switches s, t. Fix consistency
-
-def inverse_mod(a, n):
+def inverse_mod(a, n, steps=True):
     ''' Returns the multiplicative inveres of "a (mod n)" with steps '''
+
     if gcd(a, n) != 1:
-    	return f'Da gcd({a}, {n}) =/= 1, er der jvf. "Sætning 5.6" ikke en multiplikativ inveres af {a} modulus {n}'
+    	return f'As gcd({a}, {n}) =/= 1, there does not exist a multiplicative inverse of {a} modulus {n} by per "Theorem 5.6"' if steps else None
 
     # If gcd(a, n) = 1, then the muliplicative inverse is given t from Bezouts identity
     table = egcd(a, n)
@@ -15,4 +15,4 @@ def inverse_mod(a, n):
     # Get second-to-last row
     k, r, s, t = table[-2]
 
-    return f'Da {s}*{a} + {t}*{n} = {1} følger det at {s}*{a} ≡ 1 (mod {n}) '
+    return f'Da {s}*{a} + {t}*{n} = {1} følger det at {s}*{a} ≡ 1 (mod {n}) ' if steps else s
