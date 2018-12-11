cd887a72cf8b79ea7763b0abdb2e7e3261a02c69
Vincent Olesen <vincolesen@gmail.com>
2018-12-11 12:42:53 +0100

    Added congruence equations to cli

--- dtudiscrete/cli.py
+++ dtudiscrete/cli.py
@@ -2,11 +2,20 @@
 import click
 from .pretty_print import pretty_print_table
 from .least_multiple import lcm
-
+from dtudiscrete.congruence_eqn import congruence_eqn
 
 @click.group()
 def cli():
-	pass
+    pass
+
+
+@cli.command()
+@click.argument('a', type=int)
+@click.argument('b', type=int)
+@click.argument('n', type=int)
+def ceqn(a, b, n):
+    click.echo(congruence_eqn(a, b, n, steps = True).encode('utf-8'))
+
 
 @cli.command()
 @click.argument('a', type=int)
@@ -17,7 +26,5 @@ def egcd(a, b, use_latex):
     click.echo('\n' + lcm(a, b))
 
 
-
-# Execute CLI
-if __name__ == "__main__" :
-	egcd()
+if __name__ == '__main__':
+    ceqn()
--- dtudiscrete/congruence_eqn.py
+++ dtudiscrete/congruence_eqn.py
@@ -1,10 +1,10 @@
 from math import gcd
 from .extended_euclidean import egcd
 from .inverse import inverse_mod
-
+from .pretty_print import pretty_print_table
 # TODO: Notes use egcd(n, a) which switches s, t. Fix consistency
 
-def congruence_eqn(a, b, n, steps = True):
+def congruence_eqn(a, b, n, steps = True,  explanation = ''):
     '''
         This function solves for x in :math:`x \cdot a \equiv b \mod n`.
         
@@ -17,26 +17,31 @@ def congruence_eqn(a, b, n, steps = True):
         
         :rtype: str
     '''
-
     d = gcd(a, n)
 
     # Base case
     if d == 1:
+        explanation += 'Determining gcd(a, n):\n'
+
+        explanation += pretty_print_table(a, n) + '\n'
+
         c = inverse_mod(a, n, steps = False)
+
+        explanation +=  f'By "Theorem 5.8" the equation is equivalent to x ≡ c*b (mod n) where c={c} is the multiplicative inverse of a modulus n. Thus x ≡ {c}*{b} ≡ {c * b} ≡ {(c * b) % n} (mod {n}) \n'
         
-        if steps :
-            return f'By "Theorem 5.8" the equation is equivalent to x ≡ c*b (mod n) where c={c} is the multiplicative inverse of a modulus n. Thus x ≡ {c * b} ≡ {(c * b) % n} (mod {n})'
-        else :
-            return c
+        return explanation if steps else c
+
 
     # If d does not divide a, there are no solutions
     if d//b != 0:
-        return f'For d = gcd({a}, {n}), as d does not divide b, there exists no solutions per "Theorem 5.7"'
+        explanation += f'For d = gcd({a}, {n}), as d does not divide b, there exists no solutions per "Theorem 5.7"\n'
+        return explanation if steps else None
 
     # If d divides b, the eqation is equivalent to solving a' ≡ b' (mod n')
     a_prime = a//d
     b_prime = b//d
     n_prime = n//d
 
-    return congruence_eqn(a_prime, b_prime, n_prime, steps=steps)
+    explanation += f'As d = gcd(a, n) = {d} divides b, the eqation is equivalent to solving, a* ≡ b* (mod n*), where a* = a/d = {a}/{d} = {a//d}, b* = b/d = {b}/{d} = {b//d}, n* = n/d = {n}/{d} = {n//d}. \n - Trying to solve for {a_prime} ≡ {b_prime} (mod {n_prime}):\n'
 
+    return congruence_eqn(a_prime, b_prime, n_prime, steps=steps, explanation = explanation)
