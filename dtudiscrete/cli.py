609dc568d258d69ea074849b0ccf200d724018d5
Vincent Olesen <vincolesen@gmail.com>
2018-12-11 12:51:03 +0100

    Added CRT without egcd

--- dtudiscrete/cli.py
+++ dtudiscrete/cli.py
@@ -2,12 +2,22 @@
 import click
 from .pretty_print import pretty_print_table
 from .least_multiple import lcm
-from dtudiscrete.congruence_eqn import congruence_eqn
+from .congruence_eqn import congruence_eqn
+from .congruence_eqn_sys import chinese_remainder
+
 
 @click.group()
 def cli():
     pass
 
+
+@cli.command()
+@click.argument('a', type=int)
+@click.argument('b', type=int)
+@click.option('--use-latex', is_flag = True)
+def egcd(a, b, use_latex):
+    click.echo(pretty_print_table(a, b, "latex" if use_latex else "plain"))
+    click.echo('\n' + lcm(a, b))
 
 @cli.command()
 @click.argument('a', type=int)
@@ -18,12 +28,12 @@ def ceqn(a, b, n):
 
 
 @cli.command()
-@click.argument('a', type=int)
-@click.argument('b', type=int)
-@click.option('--use-latex', is_flag = True)
-def egcd(a, b, use_latex):
-    click.echo(pretty_print_table(a, b, "latex" if use_latex else "plain"))
-    click.echo('\n' + lcm(a, b))
+@click.argument('a1', type=int)
+@click.argument('a2', type=int)
+@click.argument('n1', type=int)
+@click.argument('n2', type=int)
+def crt(a1, a2, n1, n2):
+    click.echo(chinese_remainder(a1, a2, n1, n2).encode('utf-8'))
 
 
 if __name__ == '__main__':
--- dtudiscrete/congruence_eqn_sys.py
+++ dtudiscrete/congruence_eqn_sys.py
@@ -3,17 +3,19 @@ from .extended_euclidean import egcd
 
 # TODO: Notes use egcd(n, a) which switches s, t. Fix consistency
 
-def chinese_remainder(b1, b2, n1, n2):
+def chinese_remainder(b1, b2, n1, n2, steps = True, explanation = ''):
     '''
         This function solves for the chinese remainder (TODO: Equation).
     '''
 
     if gcd(n1, n2) != 1:
-    	raise NotImplementedError
+        explanation += 'As gcd(n1, n2) =/= 1, this cannot be solved the "Chinese Remainder Theorem"'
+    else:
+        # When gcd(n1, n2) "Chinese Remainder Theorem" can be used
+        u1, u2 = egcd(n1, n2)[-2][2:5]
+        
+        x_p = u1*n1*b2 + u2*n2*b1
 
-    # When gcd(n1, n2) "Chinese Remainder Theorem" can be used
-    u1, u2 = egcd(n1, n2)[-2][2:5]
+        explanation += f'By the "Chinese Remainder Theorem", as gcd(n1,n2)=1, the equations can be reduced to x ≡ u1*n1*b2 + u2*n2*b1 (mod n1*n2), which is x ≡ {x_p} ≡ {x_p % (n1*n2)} (mod {n1*n2})'
 
-    x_p = u1*n1*b2 + u2*n2*b1
-
-    return f'By the "Chinese Remainder Theorem", as gcd(n1,n2)=1, the equations can be reduced to x ≡ u1*n1*b2 + u2*n2*b1 (mod n1*n2), which is x ≡ {x_p} ≡ {x_p % (n1*n2)} (mod {n1*n2})'
+    return explanation
